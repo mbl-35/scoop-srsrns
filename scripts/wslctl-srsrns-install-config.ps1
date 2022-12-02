dd9e84bb0b2ccd5dd13e91c2ddf3d6ddcdf2aab6
mbl-35 <math.boyaval@gmail.com>
2022-12-02 10:03:22 +0100

    update with new domain

--- scripts/wslctl-srsrns-install-config.ps1
+++ scripts/wslctl-srsrns-install-config.ps1
@@ -2,5 +2,5 @@
 
 wslctl version default 2
 wslctl registry add canonical https://raw.githubusercontent.com/mbl-35/wslctl-main/main
-wslctl registry add srsrns https://store.rns.seres.lan/wsl/srsrns
+wslctl registry add srsrns https://store.seres.dev/wsl/srsrns
 wslctl registry update
